aff99ef22e06b89ed8ab9871b8e2e42fb8c5b947
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2024-06-06 11:59:42 -0400

    Switch to torch-cpu in bazel workspace.

--- third_party/resolve_requirements.sh
+++ third_party/resolve_requirements.sh
@@ -6,6 +6,10 @@ VENV_DIR="/tmp/venv-$(date +%Y%m%d-%H%M%S)"
 python3 -m venv ${VENV_DIR}
 
 source ${VENV_DIR}/bin/activate
+
+python3 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
 python3 -m pip install -U -r requirements.txt
-python3 -m pip freeze -r requirements.txt > requirements_lock.txt
+
+echo "--extra-index-url https://download.pytorch.org/whl/cpu" > requirements_lock.txt
+python3 -m pip freeze -r requirements.txt >> requirements_lock.txt
 deactivate
--- third_party/setup_toolchain.sh
+++ third_party/setup_toolchain.sh
@@ -8,14 +8,6 @@ sudo apt install -y \
     python3-venv \
     wget
 
-# Install PyTorch, refer to https://pytorch.org/get-started/locally
-# It is too big to be packed with the Bazel workspace.
-if ! [ -x "$( command -v nvidia-smi )" ]; then
-    python3 -m pip install torch torchvision
-else
-    python3 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-fi
-
 # Install Bazelisk.
 sudo wget -O /usr/bin/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-linux-amd64
 sudo chmod a+x /usr/bin/bazelisk
